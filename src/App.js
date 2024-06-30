c3242e56509e2c78633e07d90d77e09e2e4b99de
fuad <ahmadufuadoorakam@gmail.com>
2024-06-30 08:49:20 +0530

    first commit works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { db } from './firebase/config'; // Adjust the path based on your file structure
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={() => {
+        db.firestore().collection('product').get().then(snapshot => {
+          snapshot.forEach((obj) => {
+            console.log(obj.data());
+          });
+        });
+      }} > Click</button>
+    </div >
   );
 }
 
 export default App;
+
+
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,17 @@
-import firebase from "firebase";
+
+import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLmIr67M4aAoxJ-0BE3v50ObQg0UdegFo",
-    authDomain: "fir-54785.firebaseapp.com",
-    projectId: "fir-54785",
-    storageBucket: "fir-54785.appspot.com",
-    messagingSenderId: "38141617015",
-    appId: "1:38141617015:web:8caf903c02370a360ff772",
-    measurementId: "G-CBXKQTK771"
+  apiKey: "AIzaSyCLmIr67M4aAoxJ-0BE3v50ObQg0UdegFo",
+  authDomain: "fir-54785.firebaseapp.com",
+  projectId: "fir-54785",
+  storageBucket: "fir-54785.appspot.com",
+  messagingSenderId: "38141617015",
+  appId: "1:38141617015:web:8caf903c02370a360ff772",
+  measurementId: "G-CBXKQTK771"
 };
 
-export const firebase = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export default db;
